33e301924b928d651fd88442b58a6588aa524732
karthick-slg1949 <karthikarthick1949@gmail.com>
2024-07-02 14:27:19 +0530

    tst

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom'
-import App from './App'
-
-let root = ReactDOM.createRoot(document.getElementById('root'))
-
-root.render(<App/>)
-
-//ReactDOM.render("what you want to render", "where you want render")
