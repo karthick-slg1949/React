5bf075c3ea0a64fcb744eb122d2b3e77d646c65c
karthick-slg1949 <karthikarthick1949@gmail.com>
2024-07-02 15:55:12 +0530

    tst

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Message from "./Message"
+let App = ()=>{
+    return <div>
+            <h1>Welcome to the react world....</h1>  
+            <hr/>
+            <Message/>
+           </div>
+           
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom'
+import App from './App'
+// ReactDOM.render("Hello EveryOne",document.getElementById('karthi'))
+// ReactDOM.render(<h1>Hello</h1>,document.getElementById('karthi'))
+// ReactDOM.render(<h1>Hello</h1><h2></h2>,document.getElementById('karthi'))
+// ReactDOM.render(<div><h1>Hello</h1><h2>EveryOne</h2>Welcome</div>,document.getElementById('karthi'))
+
+ReactDOM.render(<App/>,document.getElementById('karthi'))
